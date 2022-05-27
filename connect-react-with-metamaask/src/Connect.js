a062084c810f20ce9ed48a9f157529391a38878a
naresh63 <nvalvi63@gmail.com>
2022-05-27 10:58:57 +0530

    react connection with metamask

---
+++ connect-react-with-metamaask/src/Connect.js
@@ -0,0 +1,12 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export const Connect = () => {
+  return(
+    <div className="connectbtn-wrapper">
+        <h1> connect react-app with metamask </h1>
+        <ConnectButton />;
+    </div>
+  )
+     
+  
+};
